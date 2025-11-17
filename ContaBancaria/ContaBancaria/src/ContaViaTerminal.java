19ffb0eba04341afce38e90a27e53666bb4ce21b
Fábio Kaio <fabiokaio501@gmail.com>
2025-11-17 11:47:36 -0300

    Projeto de Conta Via Terminal concluído.

--- ContaBancaria/ContaBancaria/src/ContaViaTerminal.java
+++ ContaBancaria/ContaBancaria/src/ContaViaTerminal.java
@@ -13,23 +13,16 @@ public class ContaViaTerminal{
         System.out.println("Digite nome: ");
             System.out.println("Nome = ");
             String nome = input.nextLine();
-                //  System.out.println("Seu nome está correto ? (Digite 'S' para Sim ou 'N' para Não :" + nome);
-                //     String confirmacaoNome = input.nextLine();
-                //     if(confirmacaoNome == "N" || confirmacaoNome =="n"){
-                //         System.out.println("Digite seu nome novamente: ");
-                //         String nomeNovo = input.nextLine();
-                //     } 
+            
             System.out.println("Agora, digite seu sobrenome: ");
                 System.out.println("Sobrenome = ");
                 String sobrenome = input.nextLine();
-                // System.out.println("Seu sobrenome está correto ? (Digite 'S' para Sim ou 'N' para Não :" + nome);
-                // String confirmacaoSobreome = input.nextLine();
-                // if(confirmacaoSobreome == "N" || confirmacaoSobreome =="n"){
-                    //     System.out.println("Digite seu sobrenome novamente: ");
-                    //     String sobrenomeNovo = input.nextLine();
-                    // }
+               
+        double saldoEmConta = 35000000.00;
                     
-                    System.out.printf("Seja muito bem-vindo %s %s",nome,sobrenome);
+        System.out.printf("Olá %s %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %f já está disponível para saque",nome,sobrenome,agenciaDaConta,numeroDaConta,saldoEmConta);
+
+        // 
         input.close();
     }
 }
